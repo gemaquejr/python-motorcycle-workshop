2e08983fcbf8dda605fa856ecb1162460bf46f0f
gemaquejr <gemaquejr@hotmail.com>
2024-09-07 22:27:02 -0300

    Adiciona aplicações no settings.py e cria os modelos para as tabelas
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- consertos/models.py
+++ consertos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from mecanicos.models import Mecanico
 
-# Create your models here.
+
+class TipoConserto(models.Model):
+    tempo_estimado = models.IntegerField()
+
+
+class ConsertoDeMoto(models.Model):
+    complexidade_do_conserto = models.IntegerField()
+    tipo_conserto = models.ForeignKey(TipoConserto, on_delete=models.CASCADE)
+    tempo_real = models.IntegerField(null=True, blank=True)
+    data_entrada = models.DateField()
+    mecanico = models.ForeignKey(Mecanico, null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"Moto {self.id} - Conserto {self.tipo_conserto}"
--- mecanicos/models.py
+++ mecanicos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Mecanico(models.Model):
+    nome = models.CharField(max_length=100)
+    idade = models.IntegerField()
+    tempo_por_dia = models.IntegerField(default=8)
+    nivel_complexidade = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
