11e065de8ae8d0027005d1b3235caacf1487eb46
gemaquejr <gemaquejr@hotmail.com>
2024-09-08 08:20:23 -0300

    Adicionando as views
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- consertos/views.py
+++ consertos/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Conserto
 
-# Create your views here.
+
+def listar_consertos(request):
+    consertos = Conserto.objects.all()
+    return render(request, 'consertos/listar_consertos.html', {'consertos': consertos})
+
+
+def detalhes_conserto(request, conserto_id):
+    conserto = get_object_or_404(Conserto, pk=conserto_id)
+    return render(request, 'consertos/detalhes_conserto.html', {'conserto': conserto})
--- mecanicos/views.py
+++ mecanicos/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Mecanico
 
-# Create your views here.
+
+def listar_mecanicos(request):
+    mecanicos = Mecanico.objects.all()
+    return render(request, 'mecanicos/listar_mecanicos.html', {'mecanicos': mecanicos})
+
+
+def detalhes_mecanico(request, mecanico_id):
+    mecanico = get_object_or_404(Mecanico, pk=mecanico_id)
+    return render(request, 'mecanicos/detalhes_mecanico.html', {'mecanico': mecanico})
